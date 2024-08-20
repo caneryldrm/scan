658276a85f6223cf3eb0306832da8b5ab024876b
Caner <caner.yildirim@tirport.com>
2024-08-20 08:41:17 +0300

    base

---
+++ src/main/java/com/migscan/migscan/MigscanApplication.java
@@ -0,0 +1,13 @@
+package com.migscan.migscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migscan/migscan/MigscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migscan.migscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
